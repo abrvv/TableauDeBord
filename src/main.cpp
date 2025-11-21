6ff30ce61050727a8377411d1e118b829ab56f2c
wwcrmnnn <31879980-wwcrmnnn@users.noreply.replit.com>
2025-11-21 10:53:26 +0000

    TP3 Fini

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 
 using namespace std;
 
-int i;
+unsigned char i;
 
 int main() { 
-
+       do{
              cout << "\t **************** Menu *************** \n";
              cout << "\n \t 1 - Multimédia \n";
              cout << "\t 2 - Téléphone \n";
@@ -19,17 +19,28 @@ int main() {
 
              switch (i){
 
-                 case 1:
+              case '1':
                     cout << "\n \t Vous avez choisi Multimédia \n";
                     break;
-             case 2:
+              case '2':
                     cout << "\n \t Vous avez choisi Téléphone \n";
                     break;
-             case 3:
+              case '3':
                     cout << "\n \t Vous avez choisi Connexions \n";
                     break;
-             case 4:
+              case '4':
                     cout << "\n \t Vous avez choisi Personnalisation - Configuration \n";
+                     do{
+                         cout << "\n \t 1 - Définir les paramètres du véhicule \n";
+                         cout << "\t 2 - Personnalisation \n";
+                         cout << "\t 3 - Configuration afficheur \n";
+
+                         cout << "\n \t Votre choix : ";
+                         cin >> i;
+                     
+                     }while (i != 'R');
                     break;
-             default: cout << "\n \t Erreur de saisie\n";}
+             ;}
+              
+       }while (i != '0');
 }
